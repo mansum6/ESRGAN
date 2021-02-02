b24f4c1fbca1e116b31ecd36084e6d9b688de3fc
efonte <3957463+efonte@users.noreply.github.com>
2021-02-02 10:58:37 +0100

    Add python types

--- upscale.py
+++ upscale.py
@@ -4,6 +4,9 @@ import math
 import os.path
 import sys
 from collections import OrderedDict
+from pathlib import Path
+from enum import Enum
+from typing import List, Optional, Union
 
 import cv2
 import numpy as np
@@ -12,17 +15,33 @@ import torch
 import utils.architecture as arch
 import utils.dataops as ops
 
-def check_model_path(model_path):
-    if os.path.exists(model_path):
+
+def check_model_path(model_path: str) -> str:
+    if Path(model_path).is_file():
         return model_path
-    elif os.path.exists(os.path.join('./models/', model_path)):
-        return os.path.join('./models/', model_path)
+    elif Path('./models/').joinpath(model_path).is_file():
+        return str(Path('./models/').joinpath(model_path))
     else:
-        print('Error: Model [{:s}] does not exist.'.format(model_path))
+        print(f'Error: Model [{model_path}] does not exist.')
         sys.exit(1)
 
+
+class SeamlessOptions(str, Enum):
+    tile = 'tile'
+    mirror = 'mirror'
+    replicate = 'replicate'
+    alpha_pad = 'alpha_pad'
+
+
+class AlphaOptions(str, Enum):
+    no_alpha = 'no_alpha'
+    bas = 'bas'
+    alpha_separately = 'alpha_separately'
+    swapping = 'swapping'
+
+
 class Upscale():
-    model = None
+    model_str = None
     input = None
     output = None
     reverse = None
@@ -37,21 +56,21 @@ class Upscale():
     alpha_boundary_offset = None
     alpha_mode = None
 
-    device = None
-    in_nc = None
-    out_nc = None
-    last_model = None
-    last_in_nc = None
-    last_out_nc = None
-    last_nf = None
-    last_nb = None
-    last_scale = None
-    last_kind = None
-    model = None
+    device: torch.device = None
+    in_nc: int = None
+    out_nc: int = None
+    last_model: str = None
+    last_in_nc: int = None
+    last_out_nc: int = None
+    last_nf: int = None
+    last_nb: int = None
+    last_scale: int = None
+    last_kind: str = None
+    model: Union[arch.nn.Module, arch.RRDB_Net, arch.SPSRNet] = None
 
-    def __init__(self, model, input, output, reverse, skip_existing,
+    def __init__(self, model: str, input, output, reverse, skip_existing,
                  seamless, cpu, device_id, cache_max_split_depth, binary_alpha,
-                  ternary_alpha, alpha_threshold, alpha_boundary_offset, alpha_mode) :
+                 ternary_alpha, alpha_threshold, alpha_boundary_offset, alpha_mode) -> None:
         self.model_str = model
         self.input = input
         self.output = output
@@ -67,16 +86,19 @@ class Upscale():
         self.alpha_boundary_offset = alpha_boundary_offset
         self.alpha_mode = alpha_mode
 
-    def run(self):
-        model_chain = self.model_str.split('+') if '+' in self.model_str else self.model_str.split('>')
+    def run(self) -> None:
+        model_chain = self.model_str.split(
+            '+') if '+' in self.model_str else self.model_str.split('>')
 
         for idx, model in enumerate(model_chain):
 
-            interpolations = model.split('|') if '|' in self.model_str else model.split('&')
+            interpolations = model.split(
+                '|') if '|' in self.model_str else model.split('&')
 
             if len(interpolations) > 1:
                 for i, interpolation in enumerate(interpolations):
-                    interp_model, interp_amount = interpolation.split('@') if '@' in interpolation else interpolation.split(':') 
+                    interp_model, interp_amount = interpolation.split(
+                        '@') if '@' in interpolation else interpolation.split(':')
                     interp_model = check_model_path(interp_model)
                     interpolations[i] = f'{interp_model}@{interp_amount}'
                 model_chain[idx] = '&'.join(interpolations)
@@ -84,13 +106,13 @@ class Upscale():
                 model_chain[idx] = check_model_path(model)
 
         if not os.path.exists(self.input):
-            print('Error: Folder [{:s}] does not exist.'.format(self.input))
+            print(f'Error: Folder [{self.input}] does not exist.')
             sys.exit(1)
         elif os.path.isfile(self.input):
-            print('Error: Folder [{:s}] is a file.'.format(self.input))
+            print(f'Error: Folder [{self.input}] is a file.')
             sys.exit(1)
         elif os.path.isfile(self.output):
-            print('Error: Folder [{:s}] is a file.'.format(self.output))
+            print(f'Error: Folder [{self.output}] is a file.')
             sys.exit(1)
         elif not os.path.exists(self.output):
             os.mkdir(self.output)
@@ -105,7 +127,7 @@ class Upscale():
             's' if len(model_chain) > 1 else '',
             ', '.join([os.path.splitext(os.path.basename(x))[0] for x in model_chain])))
 
-        images=[]
+        images = []
         for root, _, files in os.walk(input_folder):
             for file in sorted(files, reverse=self.reverse):
                 if file.split('.')[-1].lower() in ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'tiff', 'tga']:
@@ -113,7 +135,7 @@ class Upscale():
 
         # Store the maximum split depths for each model in the chain
         # TODO: there might be a better way of doing this but it's good enough for now
-        split_depths =  {}
+        split_depths = {}
 
         for idx, path in enumerate(images, 1):
             base = os.path.splitext(os.path.relpath(path, input_folder))[0]
@@ -121,8 +143,8 @@ class Upscale():
             os.makedirs(output_dir, exist_ok=True)
             print(idx, base)
             if self.skip_existing and os.path.isfile(
-                os.path.join(output_folder, '{:s}.png'.format(base))):
-                print(" == Already exists, skipping == ")
+                    os.path.join(output_folder, '{:s}.png'.format(base))):
+                print(' == Already exists, skipping == ')
                 continue
             # read image
             img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
@@ -133,24 +155,29 @@ class Upscale():
             if self.seamless == 'tile':
                 img = cv2.copyMakeBorder(img, 16, 16, 16, 16, cv2.BORDER_WRAP)
             elif self.seamless == 'mirror':
-                img = cv2.copyMakeBorder(img, 16, 16, 16, 16, cv2.BORDER_REFLECT_101)
+                img = cv2.copyMakeBorder(
+                    img, 16, 16, 16, 16, cv2.BORDER_REFLECT_101)
             elif self.seamless == 'replicate':
-                img = cv2.copyMakeBorder(img, 16, 16, 16, 16, cv2.BORDER_REPLICATE)
+                img = cv2.copyMakeBorder(
+                    img, 16, 16, 16, 16, cv2.BORDER_REPLICATE)
             elif self.seamless == 'alpha_pad':
-                img = cv2.copyMakeBorder(img, 16, 16, 16, 16, cv2.BORDER_CONSTANT, value=[0, 0, 0, 0])
+                img = cv2.copyMakeBorder(
+                    img, 16, 16, 16, 16, cv2.BORDER_CONSTANT, value=[0, 0, 0, 0])
             final_scale = 1
 
             for i, model_path in enumerate(model_chain):
-                
+
                 img_height, img_width = img.shape[:2]
 
                 # Load the model so we can access the scale
                 self.load_model(model_path)
 
                 if self.cache_max_split_depth and len(split_depths.keys()) > 0:
-                    rlt, depth = ops.auto_split_upscale(img, self.upscale, self.last_scale, max_depth=split_depths[i])
+                    rlt, depth = ops.auto_split_upscale(
+                        img, self.upscale, self.last_scale, max_depth=split_depths[i])
                 else:
-                    rlt, depth = ops.auto_split_upscale(img, self.upscale, self.last_scale)
+                    rlt, depth = ops.auto_split_upscale(
+                        img, self.upscale, self.last_scale)
                     split_depths[i] = depth
 
                 final_scale *= self.last_scale
@@ -161,10 +188,10 @@ class Upscale():
             if self.seamless:
                 rlt = self.crop_seamless(rlt, final_scale)
 
-            cv2.imwrite(os.path.join(output_folder, '{:s}.png'.format(base)), rlt)
+            cv2.imwrite(os.path.join(output_folder, f'{base}.png'), rlt)
 
     # This code is a somewhat modified version of BlueAmulet's fork of ESRGAN by Xinntao
-    def process(self,img):
+    def process(self, img: np.ndarray):
         '''
         Does the processing part of ESRGAN. This method only exists because the same block of code needs to be ran twice for images with transparency.
 
@@ -191,16 +218,18 @@ class Upscale():
         output = np.transpose(output, (1, 2, 0))
         return output
 
-    def load_model(self,model_path):
+    def load_model(self, model_path: str):
         if model_path != self.last_model:
-            if (':' in model_path or '@' in model_path) and ('&' in model_path or '|' in model_path): # interpolating OTF, example: 4xBox:25&4xPSNR:75
+            # interpolating OTF, example: 4xBox:25&4xPSNR:75
+            if (':' in model_path or '@' in model_path) and ('&' in model_path or '|' in model_path):
                 interps = model_path.split('&')[:2]
                 model_1 = torch.load(interps[0].split('@')[0])
                 model_2 = torch.load(interps[1].split('@')[0])
                 state_dict = OrderedDict()
                 for k, v_1 in model_1.items():
                     v_2 = model_2[k]
-                    state_dict[k] = (int(interps[0].split('@')[1]) / 100) * v_1 + (int(interps[1].split('@')[1]) / 100) * v_2
+                    state_dict[k] = (int(interps[0].split('@')[1]) / 100) * \
+                        v_1 + (int(interps[1].split('@')[1]) / 100) * v_2
             else:
                 state_dict = torch.load(model_path)
 
@@ -266,10 +295,10 @@ class Upscale():
             if self.in_nc != self.last_in_nc or self.out_nc != self.last_out_nc or nf != self.last_nf or nb != self.last_nb or upscale != self.last_scale or kind != self.last_kind:
                 if kind == 'ESRGAN':
                     self.model = arch.RRDB_Net(self.in_nc, self.out_nc, nf, nb, gc=32, upscale=upscale, norm_type=None, act_type='leakyrelu',
-                                        mode='CNA', res_scale=1, upsample_mode='upconv')
+                                               mode='CNA', res_scale=1, upsample_mode='upconv')
                 elif kind == 'SPSR':
                     self.model = arch.SPSRNet(self.in_nc, self.out_nc, nf, nb, gc=32, upscale=upscale, norm_type=None, act_type='leakyrelu',
-                                        mode='CNA', upsample_mode='upconv')
+                                              mode='CNA', upsample_mode='upconv')
                 self.last_in_nc = self.in_nc
                 self.last_out_nc = self.out_nc
                 self.last_nf = nf
@@ -286,7 +315,7 @@ class Upscale():
             self.model = self.model.to(self.device)
 
     # This code is a somewhat modified version of BlueAmulet's fork of ESRGAN by Xinntao
-    def upscale(self,img):
+    def upscale(self, img: np.ndarray) -> np.ndarray:
         '''
         Upscales the image passed in with the specified model
 
@@ -322,7 +351,7 @@ class Upscale():
                 output1 = self.process(img1)
                 output2 = self.process(img2)
                 output = cv2.merge(
-                    (output1[:, :, 0], output1[:, :, 1], output1[:, :, 2], output2[:, :, 0])) 
+                    (output1[:, :, 0], output1[:, :, 1], output1[:, :, 2], output2[:, :, 0]))
             # Use the alpha channel like a regular channel
             elif self.alpha_mode == 3:
                 img1 = cv2.merge((img[:, :, 0], img[:, :, 1], img[:, :, 2]))
@@ -330,7 +359,7 @@ class Upscale():
                 output1 = self.process(img1)
                 output2 = self.process(img2)
                 output = cv2.merge(
-                    (output1[:, :, 0], output1[:, :, 1], output1[:, :, 2], output2[:, :, 2])) 
+                    (output1[:, :, 0], output1[:, :, 1], output1[:, :, 2], output2[:, :, 2]))
             # Remove alpha
             else:
                 img1 = np.copy(img[:, :, :3])
@@ -340,18 +369,20 @@ class Upscale():
             if self.binary_alpha:
                 alpha = output[:, :, 3]
                 threshold = self.alpha_threshold
-                _, alpha = cv2.threshold(alpha, threshold, 1, cv2.THRESH_BINARY)
+                _, alpha = cv2.threshold(
+                    alpha, threshold, 1, cv2.THRESH_BINARY)
                 output[:, :, 3] = alpha
             elif self.ternary_alpha:
                 alpha = output[:, :, 3]
                 half_transparent_lower_bound = self.alpha_threshold - self.alpha_boundary_offset
                 half_transparent_upper_bound = self.alpha_threshold + self.alpha_boundary_offset
-                alpha = np.where(alpha < half_transparent_lower_bound, 0, np.where(alpha <= half_transparent_upper_bound, .5, 1))
+                alpha = np.where(alpha < half_transparent_lower_bound, 0, np.where(
+                    alpha <= half_transparent_upper_bound, .5, 1))
                 output[:, :, 3] = alpha
         else:
             if img.ndim == 2:
                 img = np.tile(np.expand_dims(img, axis=2),
-                                (1, 1, min(self.last_in_nc, 3)))
+                              (1, 1, min(self.last_in_nc, 3)))
             if img.shape[2] > self.last_in_nc:  # remove extra channels
                 print('Warning: Truncating image channels')
                 img = img[:, :, :self.last_in_nc]
@@ -364,7 +395,7 @@ class Upscale():
 
         return output
 
-    def crop_seamless(self,img, scale):
+    def crop_seamless(self, img: np.ndarray, scale: int) -> np.ndarray:
         img_height, img_width = img.shape[:2]
         y, x = 16 * scale, 16 * scale
         h, w = img_height - (32 * scale), img_width - (32 * scale)
@@ -372,36 +403,36 @@ class Upscale():
         return img
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('model')
-parser.add_argument('--input', default='input', help='Input folder')
-parser.add_argument('--output', default='output', help='Output folder')
-parser.add_argument('--reverse', help='Reverse Order', action="store_true")
-parser.add_argument('--skip_existing', action="store_true",
-                    help='Skip existing output files')
-parser.add_argument('--seamless', nargs='?', choices=['tile', 'mirror', 'replicate', 'alpha_pad'], default=None,
-                    help='Helps seamlessly upscale an image. Tile = repeating along edges. Mirror = reflected along edges. Replicate = extended pixels along edges. Alpha pad = extended alpha border.')
-parser.add_argument('--cpu', action='store_true',
-                    help='Use CPU instead of CUDA')
-parser.add_argument('--device_id', help='The numerical ID of the GPU you want to use. Defaults to 0.', 
-                    type=int, nargs='?', default=0)
-parser.add_argument('--cache_max_split_depth', action='store_true',
-                    help='Caches the maximum recursion depth used by the split/merge function. Useful only when upscaling images of the same size.')
-parser.add_argument('--binary_alpha', action='store_true',
-                    help='Whether to use a 1 bit alpha transparency channel, Useful for PSX upscaling')
-parser.add_argument('--ternary_alpha', action='store_true',
-                    help='Whether to use a 2 bit alpha transparency channel, Useful for PSX upscaling')
-parser.add_argument('--alpha_threshold', default=.5,
-                    help='Only used when binary_alpha is supplied. Defines the alpha threshold for binary transparency', type=float)
-parser.add_argument('--alpha_boundary_offset', default=.2,
-                    help='Only used when binary_alpha is supplied. Determines the offset boundary from the alpha threshold for half transparency.', type=float)
-parser.add_argument('--alpha_mode', help='Type of alpha processing to use. 0 is no alpha processing. 1 is BA\'s difference method. 2 is upscaling the alpha channel separately (like IEU). 3 is swapping an existing channel with the alpha channel.', 
-                    type=int, nargs='?', choices=[0, 1, 2, 3], default=0)
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model')
+    parser.add_argument('--input', default='input', help='Input folder')
+    parser.add_argument('--output', default='output', help='Output folder')
+    parser.add_argument('--reverse', help='Reverse Order', action="store_true")
+    parser.add_argument('--skip_existing', action="store_true",
+                        help='Skip existing output files')
+    parser.add_argument('--seamless', nargs='?', choices=['tile', 'mirror', 'replicate', 'alpha_pad'], default=None,
+                        help='Helps seamlessly upscale an image. Tile = repeating along edges. Mirror = reflected along edges. Replicate = extended pixels along edges. Alpha pad = extended alpha border.')
+    parser.add_argument('--cpu', action='store_true',
+                        help='Use CPU instead of CUDA')
+    parser.add_argument('--device_id', help='The numerical ID of the GPU you want to use. Defaults to 0.',
+                        type=int, nargs='?', default=0)
+    parser.add_argument('--cache_max_split_depth', action='store_true',
+                        help='Caches the maximum recursion depth used by the split/merge function. Useful only when upscaling images of the same size.')
+    parser.add_argument('--binary_alpha', action='store_true',
+                        help='Whether to use a 1 bit alpha transparency channel, Useful for PSX upscaling')
+    parser.add_argument('--ternary_alpha', action='store_true',
+                        help='Whether to use a 2 bit alpha transparency channel, Useful for PSX upscaling')
+    parser.add_argument('--alpha_threshold', default=.5,
+                        help='Only used when binary_alpha is supplied. Defines the alpha threshold for binary transparency', type=float)
+    parser.add_argument('--alpha_boundary_offset', default=.2,
+                        help='Only used when binary_alpha is supplied. Determines the offset boundary from the alpha threshold for half transparency.', type=float)
+    parser.add_argument('--alpha_mode', help='Type of alpha processing to use. 0 is no alpha processing. 1 is BA\'s difference method. 2 is upscaling the alpha channel separately (like IEU). 3 is swapping an existing channel with the alpha channel.',
+                        type=int, nargs='?', choices=[0, 1, 2, 3], default=0)
+    args = parser.parse_args()
 
-upscale = Upscale(model=args.model, input=args.input, output=args.output, reverse=args.reverse, skip_existing=args.skip_existing,
+    upscale = Upscale(model=args.model, input=args.input, output=args.output, reverse=args.reverse, skip_existing=args.skip_existing,
                       seamless=args.seamless, cpu=args.cpu, device_id=args.device_id, cache_max_split_depth=args.cache_max_split_depth,
                       binary_alpha=args.binary_alpha, ternary_alpha=args.ternary_alpha, alpha_threshold=args.alpha_threshold,
                       alpha_boundary_offset=args.alpha_boundary_offset, alpha_mode=args.alpha_mode)
-upscale.run()
-
+    upscale.run()
